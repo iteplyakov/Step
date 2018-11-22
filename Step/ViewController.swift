d7de3ea56c75938b61ef87d16a15dec6192fdab2
Iwan Teplyakov <iteplyakov@icloud.com>
2018-11-22 15:57:23 +0200

    Initial Commit

---
+++ Step/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Step
+//
+//  Created by Iwan Teplyakov on 11/22/18.
+//  Copyright © 2018 Iwan Teplyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
